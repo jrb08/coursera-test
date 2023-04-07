1e2dd92a879906392428841137905331469c46b5
jrb08 <jrbootea@gmail.com>
2023-04-07 11:59:43 -0500

    push

--- coursera-test/js/script.js
+++
@@ -1,9 +0,0 @@
-var literalCircle = {
-    radius:10,
-    getArea: function(){
-
-        console.log(this);
-        return Math.PI * Math.pow(this.radius, 2);
-    }
-    };
-console.log(literalCircle.getArea());
